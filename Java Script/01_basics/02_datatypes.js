045b2ddce93d028ea1e445a03dda7410554e4503
Harsh Gupta <rockstar221405@gmail.com>
2025-07-28 17:44:38 +0530

    JavaScript Started by variables, datatypes, conversion

---
+++ Java Script/01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert("3 + 3") we are using nodejs ,not browser
+
+// console.log(3+3)
+// console.log("Hitesh")
+
+let name = "rockstar"
+let age = 18
+let isLoggedIn = false
+/*
+number => 2 to power 53
+BigInt
+String
+Boolean
+null
+undefined =>
+*/
+
+console.log(typeof null)
