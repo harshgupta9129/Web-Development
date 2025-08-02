f9130d7fffcf81ecf2506f4ef32b5776b640c45f
Harsh Gupta <rockstar221405@gmail.com>
2025-08-02 18:30:43 +0530

    Part First of Basics Completed

--- Java Script/01_basics/06_strings.js
+++ Java Script/01_basics/06_strings.js
@@ -8,10 +8,30 @@ const repoCount = 50
 
 const gameName = new String('Rockstar')
 
-console.log(gameName[0])
-console.log(gameName.__proto__)
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
 
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('k'))
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('k'))
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-5,5)
+// console.log(anotherString);
+
+const newStringOne = "       rockstar       "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://rockstar.com/rockstar%20rockstar"
+
+// console.log(url.replace('%20','-'))
+
+console.log(url.includes('star'))
+
+const string1 = 'Hello-my-world'
+console.log(string1.split('-')) 
+
---
+++ Java Script/01_basics/08_dates.js
@@ -0,0 +1,17 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+
+// let myCreatedDate = new Date(2023, 0,23, 5, 3, 24)
+// let myCreatedDate = new Date("2006-03-22")
+let myCreatedDate = new Date("03-22-2006")
+// console.log(myCreatedDate.toDateString())
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getDay());
+console.log(myCreatedDate.getFullYea)
