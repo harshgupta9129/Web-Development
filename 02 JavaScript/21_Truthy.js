14cba755cdd4eb24d88c525e96c639b69e8961d5
harshgupta9129 <rockstar221405@gmail.com>
2025-09-16 01:05:02 +0530

    Execution Context and Control Flow Completed

---
+++ 02 JavaScript/21_Truthy.js
@@ -0,0 +1,28 @@
+// const userEmail = "user@gmail.com"
+
+// if (userEmail) {
+//     console.log("Got user email")
+// } else {
+//     console.log("Don't Have user email")
+// }
+
+// flasy Values
+// false, 0, -0, BigInt 0n, null, undefined, NaN, ""
+
+// Truthy Values
+// "0", 'false', ' ', [] , {}, function () {}
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// console.log(val1)  // OutPut : 5
+// val2 = null ?? 10
+// console.log(val2) // Output : 10
+
+// Terniary Operator
+
+// condition ? true : false
+
+// const iceTeaPrice = 100;
+// iceTeaPrice > 80 ? console.log("More Than 80") : console.log("Less Than 80")
