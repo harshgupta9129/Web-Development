a4cc28199e51d08dd20daa4cea59e5a7b47fbb66
harshgupta9129 <rockstar221405@gmail.com>
2025-09-16 19:37:48 +0530

    Iterations Completed

---
+++ 02 JavaScript/23_Iterations.js
@@ -0,0 +1,88 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+//forof
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+// const greetings = "Hello World"
+// for (const greet of greetings) {
+//     console.log(greet)
+// }
+
+// Maps
+
+// const map = new Map ()
+// map.set('IN', 'INDIA')
+// map.set('USA', 'UNITED STATES OF AMERICA')
+// map.set('FR', 'FRANCE')
+
+// for (const [key, value] of map) {
+//     console.log(key)
+//     console.log(value)
+// }
+
+// const myObject = {
+//     game1 : 'NFS',
+//     game2 : 'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(key)
+//     console.log(value);
+// }  // object are not iterable with this loop
+
+// const myObject = {
+//     js : "JavaScript",
+//     cpp : "C++",
+//     rb : "ruby"
+// }
+
+// forin
+// for (const key in myObject) {
+//     console.log(`Full Form of ${key} is ${myObject[key]}`)
+// }
+
+const coding = ["JS", "Ruby", "Java", "Python"]
+
+// coding.forEach(function (item) {
+//     console.log(item);
+// })
+
+// coding.forEach ((item) => {
+//     console.log(item)
+// })
+
+// function printme (item) {
+//     console.log(item)
+// }
+
+// coding.forEach(printme);
+
+// coding.forEach((item ,index ,array) => {
+//     console.log(`${item} and ${index} and ${array}`)
+// }) 
+
+// const myCoding = [
+//     {
+//         languageName : "JavaScript",
+//         languageFileName : "js"
+//     },
+//     {
+//         languageName : "C Language",
+//         languageFileName : "c"
+//     },
+//     {
+//         languageName : "Python",
+//         languageFileName : "py"
+//     }
+// ]
+
+// myCoding.forEach((item) => {
+//     console.log(item.languageName)
+// })
