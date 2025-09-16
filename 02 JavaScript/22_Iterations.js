ce076a0063089302ce7160ad20e40bb2229fd9c3
harshgupta9129 <rockstar221405@gmail.com>
2025-09-16 18:47:15 +0530

    Iterations Completed

---
+++ 02 JavaScript/22_Iterations.js
@@ -0,0 +1,63 @@
+// for 
+
+// for (let index = 1; index < 10; index++) {
+//     const element = index;
+//     if (element == 5) console.log("5 is best number");
+//     console.log(element);
+// }
+
+// for (let j = 1; j <=10; j++) {
+//     console.log(`Table of ${j}`)
+//     for (let i = 1; i <=10; i++) {
+//         console.log(`${j} x ${i} = ${i*j}`)
+//     }
+// }
+
+let myarray = ["flash", "batman", "superman"]
+
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for (let index = 0; index <=20; index++) {
+//     if (index == 5) {
+//         break;
+//     }
+//     console.log(`Value of index is ${index}`)
+// }
+
+// for (let index = 0; index <=20; index++) {
+//     if (index == 5) {
+//         console.log(`${index} Detected`)
+//         continue;
+//     }
+//     console.log(`Value of index is ${index}`)
+// }
+
+// While
+
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2
+// }
+
+// let arr = 0
+// while (arr<myarray.length) {
+//     console.log(`Value is ${myarray[arr]}`)
+//     arr++;
+// }
+
+// do {
+    
+// } while (condition);
+
+// let score = 11
+// do {
+//     console.log(`Score is ${score}`)
+//     score++;
+// } while (score <=10);  // Give Output 11
+
